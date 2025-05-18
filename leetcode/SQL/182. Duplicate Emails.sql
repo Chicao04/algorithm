707274a73af9e021109a7e715e97eb7b36bc743f
Chicao04 <caocute502@gmail.com>
2025-05-18 10:20:54 +0700

    l4

---
+++ leetcode/SQL/182. Duplicate Emails.sql
@@ -0,0 +1,39 @@
+/*
+Table: Person
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+*/
+
+
+# Write your MySQL query statement below
+select email
+    from Person
+    group by email
+    having count(email) > 1;
+
+
+
